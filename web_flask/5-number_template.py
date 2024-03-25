4cd39a94249e9fa4732cd7f5b057231fe74c7e88
MACRODAT <macrodatservices@gmail.com>
2024-03-25 02:37:18 +0000

    Changes to the file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,6 +37,7 @@ def imanumber(n):
     """display “n is a number” only if n is an integer"""
     return "{:d} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbersandtemplates(n):
     """display a HTML page only if n is an integer"""
