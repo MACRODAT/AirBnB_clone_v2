3f58d04cd1bb0c35bf3219f15897c7044d86cac3
MACRODAT <macrodatservices@gmail.com>
2024-03-25 02:28:30 +0000

    Changes to the file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+web framework project
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """returns default message"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnbb():
+    """returns default message"""
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def render(text):
+    """returns default message"""
+    return 'C' + str(text).replace(' ', '_')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5000')
