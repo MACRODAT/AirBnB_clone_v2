1df948dbcb3d98ec578ae37d9affdd5a3769e56b
MACRODAT <macrodatservices@gmail.com>
2024-03-25 02:21:01 +0000

    Changes to the file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ web framework project
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """returns default message"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='5000')
